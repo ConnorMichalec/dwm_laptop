e038336e8cf19f4597bcb160fc27ebb0ecd1098e
Connor Michalec <connormichalec@gmail.com>
2021-11-24 03:31:14 -0600

    Updated vertical padding for status bar

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 5;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 5;        /* vertical padding for statusbar */
+static const int vertpadbar         = 8;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "terminus:size=11" };
 static const char dmenufont[]       = "terminus:size=11";
 static const char col_gray1[]       = "#222222";

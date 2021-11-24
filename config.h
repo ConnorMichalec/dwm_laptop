428d5c28a3dac91898d003c1ddc1a38767284c44
Connor Michalec <connormichalec@gmail.com>
2021-11-24 01:18:00 -0600

    ADDED PATCH: Statuspadding

--- config.h
+++ config.h
@@ -6,6 +6,8 @@ static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 5;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 5;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "terminus:size=11" };
 static const char dmenufont[]       = "terminus:size=11";
 static const char col_gray1[]       = "#222222";

2e28ba5e8ae27d6c2467214674962245f4576dea
Connor Michalec <connormichalec@gmail.com>
2022-05-16 03:29:55 -0500

    PATCH ADDED: Tagothermonitor
    Had to manually configure to be mod1mask instead of mod4mask as mod4mask is my current modkey(super key) so i set to mod1mask for the functions which is the alt key.

--- config.h
+++ config.h
@@ -102,7 +102,9 @@ static const Layout layouts[] = {
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} }, \
+	{ MODKEY|Mod1Mask,              KEY,      tagnextmon,     {.ui = 1 << TAG} }, \
+	{ MODKEY|Mod1Mask|ShiftMask,    KEY,      tagprevmon,     {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/usr/bin/urxvt", "-c", cmd, NULL } }
